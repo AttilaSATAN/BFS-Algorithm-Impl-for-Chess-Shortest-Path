6ea618e287902c695925de76814b2fb35e588967
Attila Satan <attila.satan@gmail.com>
2018-05-22 15:44:06 +0300

    initial

--- solution.py
+++ solution.py
@@ -1,4 +1,24 @@
 
+import math
+### Use this space to try out ideas and free code ###
+def coordinat(number):
+    y = int(math.floor(number/8))
+    x = number%8
+    return x, y
+
 def answer(src, dest):
-    # Solution to here
+    print(coordinat(src))
+    print(coordinat(dest))
+    
+# -2, -1
+# -2, +1
+# -1, -2
+# -1, +2
+# +2, +1
+# +2, -1
+# +1, +2
+# +1, -2
+
+
+answer(33,5)
 
